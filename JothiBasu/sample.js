6604255d0320ef06a32a19f99bfdc819d30c34e7
JothiBasu-Developer <jothibasu4102001@gmail.com>
2024-06-26 10:20:13 +0530

    my  java script file

---
+++ JothiBasu/sample.js
@@ -0,0 +1,56 @@
+// function Person(first,last){
+//   this.firstName=first,
+//   this.lastName=last,
+//   this .getFullName =function(){
+//       return this.firstName+" "+ this.lastName
+//   }
+// }
+// const person1=new Person('jo','jo');
+// const person2=new Person('ma','ba');
+// // person1.getFullName();
+// console.log(person2.getFullName());
+
+
+
+// function Constructor(first,last){
+//   this.firstName=first,
+//   this.lastName=last
+// }
+// const person1=new Constructor("ananthi","gunasekaran");
+// const person2=new Constructor("kaveri","guna");
+
+// console.log(person1);
+
+
+// function Constructor(){
+//   this.firstName="ananthi",
+//   this.lastName="gunasekaran"
+// }
+// const person1=new Constructor();
+
+// person1.age=24;
+// console.log(person1);
+// for loop
+// let a=['jo','josh'];
+// for(let i=0;i<a.length;i++){
+//   console.log(a[i]);
+// }
+// const obj={
+//   name:'jo',
+//   age:22
+// }
+// for(let a in obj){
+//   console.log(obj[a]);
+// }
+// let s="jothi basu";
+// console.log(s.charAt(4));
+// var st="makesh";
+// document.write(st.charAt(2));
+// document.write("the values are");
+// let color=['yello','red','pink','orange','green'];
+// color.forEach((e,index) => {
+//   e[index].concat("s");
+// }
+
+// )
+//set
