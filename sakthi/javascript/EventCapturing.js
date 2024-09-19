d97858b62649f77a4607da8d4fccab259230ffc4
Ajai-sakthi <severusblu@gmail.com>
2024-09-19 18:11:42 +0530

    Event Capturing

--- sakthi/javascript/EventCapturing.js
+++ sakthi/javascript/EventCapturing.js
@@ -1,3 +1,9 @@
+/*Event Capturing is different from Event Bubbling => 
+The third parameter of the addEventListener is optional
+and it is boolean also, the default value of the 
+boolean is false there,
+if we doesn't change anything, Event Bubbling will happen normally.
+if we change that parameter to true then Event Capturing will happen.*/
 document.querySelector("#grandparent")
 .addEventListener('click',()=>{
     console.log("Grandparent is clicked");
