9ae9510c84f6ae07cf3a98945aa2b4d60de64540
Ajai-sakthi <severusblu@gmail.com>
2024-09-19 17:56:14 +0530

    Event Bubbling

---
+++ sakthi/javascript/EventBubbling.js
@@ -0,0 +1,12 @@
+document.querySelector("#grandparent")
+.addEventListener('click',()=>{
+    console.log("Grandparent is clicked");
+})
+document.querySelector("#parent")
+.addEventListener('click',()=>{
+    console.log("Parent is clicked");
+})
+document.querySelector("#child")
+.addEventListener('click',()=>{
+    console.log("Child is clicked");
+})
