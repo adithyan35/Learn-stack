31c92f955869393359671c18dcdbbd20254dac1f
Ajai-sakthi <severusblu@gmail.com>
2024-11-12 18:43:22 +0530

    New Task Added

--- sakthi/Task/src/app/app.module.ts
+++ sakthi/Task/src/app/app.module.ts
@@ -10,14 +10,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { FormsModule } from '@angular/forms';
+import { ContentchangeComponent } from './contentchange/contentchange.component';
 @NgModule({
   declarations: [
     AppComponent,
     Snackbar1Component,
     Snackbar2Component,
-    Snackbar3Component
-  
+    Snackbar3Component,
+    ContentchangeComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,7 +26,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [
     provideAnimationsAsync()
---
+++ sakthi/Task/src/app/contentchange/contentchange.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'contentchange',
+  templateUrl: './contentchange.component.html',
+  styleUrl: './contentchange.component.scss'
+})
+export class ContentchangeComponent {
+  isInput!:string; 
+}
